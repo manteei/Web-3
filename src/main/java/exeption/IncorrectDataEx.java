ef99bfa4652fc433ed514afb7dcb37343cb65d30
manteei <95652195+manteei@users.noreply.github.com>
2022-12-02 15:54:04 +0300

    Delete src/main/java/exeption directory

--- src/main/java/exeption/IncorrectDataEx.java
+++
@@ -1,9 +0,0 @@
-package exeption;
-
-import javax.faces.application.FacesMessage;
-
-public class IncorrectDataEx extends RuntimeException {
-    public IncorrectDataEx(FacesMessage message) {
-        super(String.valueOf(message));
-    }
-}
